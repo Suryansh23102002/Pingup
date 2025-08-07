f16d151897efe66a1ce5a2ba846ef3cc8176fd4b
Suryansh Pandey <yashpandey.2310@gmail.com>
2025-08-07 23:09:39 +0530

    backend Complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,9 @@
 // Get the User Data using userId
 
 import imagekit from "../configs/imageKit.js";
+import { inngest } from "../inngest/index.js";
 import Connection from "../models/Connection.js";
+import Post from "../models/Post.js";
 import User from "../models/User.js";
 import fs from 'fs'
 
@@ -192,10 +194,16 @@ export const sendConnectionRequest = async(req, res)=>{
       ]
     })
     if(!connection){
-      await Connection.create({
+      const newConnection =  await Connection.create({
         from_user_id: userId,
         to_user_id: id
       })
+
+      await inngest.send({
+        name: 'app/connection-request',
+        data: {connectionId: newConnection._id}
+      })
+
       res.json({success: true, message: "Connection request sent successfully"})
     }else if(connection && connection.status === 'accepted'){
         res.json({success: false, message: "You are already connected with this user"})
@@ -259,5 +267,23 @@ export const acceptConnectionRequest = async(req, res)=>{
   } catch (error) {
     console.log(error);
     res.json({success: false, message: error.message})
+  }
+}
+
+// Get User Profiles
+
+export const getUserProfiles = async(req, res) => {
+  try {
+    const { profileId } = req.body;
+    const profile = await User.findById(profileId)
+    if(!profile){
+      return res.json({success: false, message: "Profile not found"})
+    }
+    const posts =  await Post.find({user: profileId}).populate('user')
+    res.json({success: true, profile, posts})
+    
+  } catch (error) {
+     console.log(error);
+     res.json({success: false, message: error.message})
   }
 }
